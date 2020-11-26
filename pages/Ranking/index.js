79b2cb539757bf0e690c4365a5c0279fac07c015
DharoOficial <erickbandeiradharo@gmail.com>
2020-11-26 11:13:09 -0300

    ranking em style ranking

--- pages/Ranking/index.js
+++ pages/Ranking/index.js
@@ -1,28 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
-import {url} from '../../utils/constants';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { url } from '../../utils/constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const Ranking = () => {
 
-    const [nota , setNota] = useState([])
+    const [nota, setNota] = useState([])
 
-    const PegarNotas =() => {
-        
+    const PegarNotas = () => {
+
         fetch(`${url}/ObjetivoAluno`, {
-            headers : {
-                'authorization' : 'Bearer ' + AsyncStorage.getItem('@jwt')
+            headers: {
+                'Content-Type': 'application/json'
             }
         })
-        .then(response => response.json())
-        .then(dados => {
-            setNota(dados);
-        })
+            .then(response => response.json())
+            .then(dados => {
+                setNota(dados);
+            })
 
     }
 
-    useEffect(() => 
-    {PegarNotas()}, )
-    
+    useEffect(() => { PegarNotas(), Ordenar() })
+
+    const Ordenar = () => {
+        nota.filter(a => {
+            return
+        })
+    }
 
     const renderItem = () => {
         return (
@@ -30,11 +34,13 @@ const Ranking = () => {
                 <Text> notas={nota.item.Nota}</Text>
             </View>
         )
-    }   
+    }
     return (
         <View style={styles.container} >
             <Text>Ranking</Text>
-            <FlatList 
+            <FlatList
+                style={styles.circulo,
+                    backgroundColor = '#000'}
                 data={nota}
                 keyExtractor={item => item.id}
                 renderItem={renderItem}
@@ -44,11 +50,16 @@ const Ranking = () => {
 }
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#f8f8f8',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    
+    circulo: {
+        height: 30,
+        width: 30,
+        borderRadius: 30,
+    },
+
 })
 export default Ranking;
