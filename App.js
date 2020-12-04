fd8301028e347e19ca0f09dd84330892380cd43b
sena-code <sena.carbone@gmail.com>
2020-12-03 21:19:51 -0300

    Ajustes

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Ranking} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Professor" component={Professor} />
         <Stack.Screen name="Aluno" component={Aluno}/>
       </Stack.Navigator>
